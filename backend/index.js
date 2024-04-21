f282c499e99bbda0d46011b5eb3191357822e97a
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2024-04-21 19:38:09 +0200

    deploy con cuidao

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const activitiesRouter = require('./activities');
 const meRouter = require('./me');
+const radioRouter = require('./radio');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //CONFIG
 global.id = '725316907187568701'; //El ID del usuario de Discord
-
+global.url = process.env.URL_RADIO; //La url de la radio
+global.logs = process.env.LOGS; //logs discord
 app.use(cors());
 
 app.use(activitiesRouter);
 app.use(meRouter);
+app.use(radioRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
