6a441c75c6200a36ff3180d321acfb36b6818136
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2024-05-02 16:32:46 +0200

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const activitiesRouter = require('./activities');
 const meRouter = require('./me');
 const radioRouter = require('./radio');
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -12,12 +14,20 @@ const PORT = process.env.PORT || 3000;
 global.id = '725316907187568701'; //El ID del usuario de Discord
 global.url = process.env.URL_RADIO; //La url de la radio
 global.logs = process.env.LOGS; //logs discord
+global.ciudad = process.env.CITY; // pues donde vives
+global.apikey = process.env.APIKEY; //api key de weatherapi.com
+global.secretredis = process.env.REDISSECRET; //redis secret
+global.hostredis = process.env.REDISHOST; //redis host
+global.portredis = process.env.REDISPORT; //redis port
+
 app.use(cors());
 
 app.use(activitiesRouter);
 app.use(meRouter);
 app.use(radioRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+
+
   console.log(`Server is running on port ${PORT}`);
 });
--- backend/me.js
+++ backend/me.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
+const { createClient } = require('redis');
 
+const client = createClient({
+    password: global.secretredis,
+    socket: {
+        host: global.hostredis,
+        port: global.portredis
+    }
+});
+
+async function loadredis() {
+  await client.connect();
+  console.log("redis cargao")
+}
+
+loadredis();
 router.get('/me', async (req, res) => {
   try {
     const response = await axios.get(`https://api.lanyard.rest/v1/users/${global.id}`);
@@ -13,12 +28,43 @@ router.get('/me', async (req, res) => {
       avatar,
       status
     };
-
+   
     res.json(responseData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
+});
+
+
+router.get('/meteo', async (req, res) => {
+  const meteoredischeck = await client.get('meteo');
+  if(meteoredischeck == null){ 
+  try{
+    const response = await axios.get("https://api.weatherapi.com/v1/current.json?key="+global.apikey+"&q="+ global.ciudad +"&aqi=no&lang=es"); //se debería de guardar en redis o algo? evidentemente pero 2 cosas: 1, nadie me va a petar esto i think, si alguien me lo peta me jodo 2, la api es bastante rápida no es necesario cachear [HECHO]
+    const temp = response.data.current.temp_c;
+    const condicion = response.data.current.condition.text;
+    const icon = response.data.current.condition.icon;
+
+    const limpio = {
+      temp,
+      condicion,
+      icon
+  }
+   
+    await client.set('meteo', JSON.stringify(limpio), {
+      EX: 300
+    });
+    res.json(limpio)
+  } catch (error){
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+else {
+  res.json(JSON.parse(meteoredischeck))
+}
+//ya que hice el coñazo del redis podría meterlo para todo lo otro pero que puta pereza macho
 });
 
 module.exports = router;
