fef2567cadb35e234684c569dfb1a1c42a564c6e
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2024-05-03 14:58:34 +0200

    a vercel no le gusta redis

--- backend/me.js
+++ backend/me.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
-const { createClient } = require('redis');
+/*const { createClient } = require('redis');
 
 const client = createClient({
     password: process.env.REDISSECRET,
@@ -17,6 +17,7 @@ async function loadredis() {
 }
 
 loadredis();
+*/ //chau redis, vercel no te quiere
 router.get('/me', async (req, res) => {
   try {
     const response = await axios.get(`https://api.lanyard.rest/v1/users/${global.id}`);
@@ -38,9 +39,9 @@ router.get('/me', async (req, res) => {
 
 
 router.get('/meteo', async (req, res) => {
-  const meteoredischeck = await client.get('meteo');
+  /*const meteoredischeck = await client.get('meteo');
   if(meteoredischeck == null){ 
-  try{
+  */try{
     const response = await axios.get("https://api.weatherapi.com/v1/current.json?key="+global.apikey+"&q="+ global.ciudad +"&aqi=no&lang=es"); //se debería de guardar en redis o algo? evidentemente pero 2 cosas: 1, nadie me va a petar esto i think, si alguien me lo peta me jodo 2, la api es bastante rápida no es necesario cachear [HECHO]
     const temp = response.data.current.temp_c;
     const condicion = response.data.current.condition.text;
@@ -52,19 +53,19 @@ router.get('/meteo', async (req, res) => {
       icon
   }
    
-    await client.set('meteo', JSON.stringify(limpio), {
+   /* await client.set('meteo', JSON.stringify(limpio), {
       EX: 300
     });
-    res.json(limpio)
+    */res.json(limpio)
   } catch (error){
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 }
-else {
+/*else {
   res.json(JSON.parse(meteoredischeck))
-}
-//ya que hice el coñazo del redis podría meterlo para todo lo otro pero que puta pereza macho
+}*/
+//ya que hice el coñazo del redis podría meterlo para todo lo otro pero que puta pereza macho VERCEL NO ME QUIERE
 });
 
 module.exports = router;
