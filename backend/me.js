a309f43b9c1df6019b526632764bf6e502f9fc61
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2024-05-15 15:07:50 +0200

    Update me.js

--- backend/me.js
+++ backend/me.js
@@ -22,14 +22,42 @@ router.get('/me', async (req, res) => {
   try {
     const response = await axios.get(`https://api.lanyard.rest/v1/users/${global.id}`);
 
+    let spotify;
+    const spotistatus = response.data.data.listening_to_spotify;
+
+    if (spotistatus === true) {
+      spotify = response.data.data.spotify;
+       if (spotify.artist && typeof spotify.artist === 'string') {
+        spotify.artist = spotify.artist.replace(/;/g, ',');
+      }
+    } else {
+      spotify = false;
+    }
+
+    let activities = response.data.data.activities;
+    // De esta forma el estado personalizado no lo devolverá la API
+    activities = activities.filter(activity => activity.name !== 'Custom Status');
+
+    let act;
+    //tambien quita spotify porque pa qué si ya lo devuelves arriba 
+    const filteredActivities = activities.filter(activity => activity.name !== 'Spotify');
+
+    if (filteredActivities.length === 0) {
+      act = false;
+    } else {
+      act = filteredActivities;
+    }
+
     const avatar = response.data.data.discord_user.avatar;
     const status = response.data.data.discord_status;
 
     const responseData = {
+      spotify,
+      act,
       avatar,
-      status
+      status,
     };
-   
+
     res.json(responseData);
   } catch (error) {
     console.error(error);
