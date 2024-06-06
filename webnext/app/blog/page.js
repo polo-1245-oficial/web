efa41a750be2bd4983900bcaebad64e7ccd7463a
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2024-06-06 18:10:32 +0200

    asd

--- webnext/app/blog/page.js
+++ webnext/app/blog/page.js
@@ -1,11 +1,15 @@
 import React from 'react';
-const App = () => {
+import Fetch from '../components/fetchPosts';
 
+const App = () => {
   return (
-      <main className='text-4xl text-white justify-center flex text-center'>
-        <p>en algún otro momento (más cercano)</p>
-      </main>
-      
+    <main className='text-xl text-white'>
+      <div className='flex justify-center items-center'>
+        <div className="max-w-5xl mx-auto md:grid-cols-3 gap-4 mt-12">
+          <Fetch />
+        </div>
+      </div>
+    </main>
   );
 };
 
