1171ce4e1b06359fcdc98fa45f6eb6a7b7beaebd
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2024-06-16 09:55:52 +0200

    Update layout.js

--- webnext/app/layout.js
+++ webnext/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-     
+     <script defer src="https://cloud.umami.is/script.js" data-website-id="d23e6310-ef31-4948-8143-a6bbc67d7a4a"></script>
       
       <body className={inter.className}>
       <Header />
